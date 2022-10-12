34029c9cadb360699e3f2d6e8d56e127435c6dae
cop-pat-bednar <115665581+cop-pat-bednar@users.noreply.github.com>
2022-10-12 15:30:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit
-
+# this will build the menu 
 streamlit.title('👌My Parents New Healthy Dinner.')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & 🥞Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍳Hard-Boiled Free-Range Egg')
 
+# add another header
 streamlit.header('🍌🍓Build your own friut smoothie')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#choose the fruit name column as the index
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a picklist here so they can pick the fruid they want to include
 streamlit.multiselect("Pick some fruits", list(my_fruit_list.index))
