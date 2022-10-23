9b38fb3e8b1b59561443d13e0cd22a1136784690
cop-pat-bednar <115665581+cop-pat-bednar@users.noreply.github.com>
2022-10-23 14:24:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ streamlit.dataframe(fruityvice_normalized)
 streamlit.header("The Fruit load list contains")
 #Snowflake-related functions
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("use warehouse pc_rivery_wh") 
         my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list;")
         return my_cur.fetchall()
