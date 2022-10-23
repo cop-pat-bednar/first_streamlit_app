c5045d429dec2f73cb0017ffd36ae25fa780e39d
cop-pat-bednar <115665581+cop-pat-bednar@users.noreply.github.com>
2022-10-23 14:43:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,10 +52,12 @@ except URLError as e:
 # output to the screen as a table
 #streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
 streamlit.header("The Fruit load list contains")
 #Snowflake-related functions
 def get_fruit_load_list():
-    with my_cur.cursor() as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("use warehouse pc_rivery_wh") 
         my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list;")
         return my_cur.fetchall()
