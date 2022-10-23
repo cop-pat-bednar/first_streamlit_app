c2b27188364a1308bb60c122ba3c3f0dbe4bac95
cop-pat-bednar <115665581+cop-pat-bednar@users.noreply.github.com>
2022-10-23 13:30:03 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,10 +26,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #Lesson 9 - #New Section to display fruityvice api response
 #create the repeatable code block (function)
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fuityvice_normalized
+#def get_fruityvice_data(this_fruit_choice):
+   # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   # return fuityvice_normalized
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -38,8 +38,10 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_fruityvice_data(this_fruit_choice)
-    streamlit.dataframe(back_from_function)
+   # back_from_function = get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()   
 
