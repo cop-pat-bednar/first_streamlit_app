ac27b8df1d9111cd81bcf37e74a3f48268044b2d
cop-pat-bednar <115665581+cop-pat-bednar@users.noreply.github.com>
2022-10-23 13:45:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
     back_from_function = get_fruityvice_data(this_fruit_choice)
     #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()   
 
